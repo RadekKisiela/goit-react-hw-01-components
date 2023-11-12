52a97108ac9c052319c9ca094ecbfef51ae26e2a
RadekKisiela <radek1997_97@o2.pl>
2023-11-13 00:35:21 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,10 @@
 import Profile from './profile/Profile';
 import Statistics from './statistics/Statistics';
 import FriendList from './friednList/FriendList';
-// import TransactionHistory from './transactions/TransactionHistory';
 
 import user from './profile/user.json';
 import data from './statistics/data.json';
 import friends from './friednList/friends.json';
-// import transactions from './transactions/transactions.json';
 
 export const App = () => {
   return (
@@ -31,7 +29,6 @@ export const App = () => {
       />
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
-      {/* <TransactionHistory items={transactions} /> */}
     </div>
   );
 };
